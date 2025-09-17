761fa8baebdc8235f822faaea69636f52bd2359b
ap-apely <nb8577454@gmail.com>
2025-09-17 22:59:31 +0300

    Revert "style: format code with Prettier and Rustfmt"
    This reverts commit bce71118202a276e40b4f14c2009b9057809863e.

--- build.rs
+++ build.rs
@@ -2,10 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sea_path = std::env::var("SEA_PATH").unwrap_or_else(|_| "../Sea".into());
 
     tonic_prost_build::compile_protos(format!("{}/api/auth/proto/account.proto", sea_path))?;
-    tonic_prost_build::compile_protos(format!(
-        "{}/service/group/proto/group_microservice.proto",
-        sea_path
-    ))?;
+    tonic_prost_build::compile_protos(format!("{}/service/group/proto/group_microservice.proto", sea_path))?;
     tonic_prost_build::compile_protos(format!("{}/service/voice/proto/signaling.proto", sea_path))?;
     tonic_prost_build::compile_protos(format!("{}/api/status/proto/user_status.proto", sea_path))?;
 
