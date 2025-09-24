c66a5a074703327dbdaa7a4a0aa66e33f186a6af
Apely <60784156+ap-apely@users.noreply.github.com>
2025-09-24 19:13:45 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sea_path = std::env::var("SEA_PATH").unwrap_or_else(|_| "../Sea".into());
 
-    tonic_prost_build::compile_protos(format!("{}/api/auth/proto/account.proto", sea_path))?;
+    tonic_prost_build::compile_protos(format!("{}/service/auth/api/auth/proto/account.proto", sea_path))?;
     tonic_prost_build::compile_protos(format!(
         "{}/service/group/proto/group_microservice.proto",
         sea_path
     ))?;
     tonic_prost_build::compile_protos(format!("{}/service/voice/proto/signaling.proto", sea_path))?;
-    tonic_prost_build::compile_protos(format!("{}/api/status/proto/user_status.proto", sea_path))?;
+    tonic_prost_build::compile_protos(format!("{}/service/auth/api/status/proto/user_status.proto", sea_path))?;
 
     tauri_build::build();
 
